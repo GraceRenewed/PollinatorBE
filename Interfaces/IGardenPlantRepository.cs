2c13cbdb7de65dbc45f24174f79d5bdf08a14c6b
Christina Vieau <cvieau@live.com>
2025-05-29 11:09:43 -0400

    made gardenPlant interfaces

---
+++ Interfaces/IGardenPlantRepository.cs
@@ -0,0 +1,13 @@
+﻿using PollinatorBE.Models;
+
+namespace PollinatorBE.Interfaces
+{
+    public interface IGardenPlantRepository
+    {
+        Task<List<GardenPlant>> GetGardenPlantsAsync();
+        Task<GardenPlant> GetGardenPlantsByIdAsync(string id);
+        Task<GardenPlant> CreateGardenPlantsAsync(GardenPlant gardenPlant);
+        Task<GardenPlant> UpdateGardenPlantAsync(string id, GardenPlant gardenPlant);
+        Task DeleteGardenPlantAsync(string id);
+    }
+}
